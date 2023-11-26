a24a46555181dc7c8a314b63c6f4770d88ec4b1e
Iñaki Pujol <ipujol10@gmail.com>
2023-11-26 12:13:53 +0100

    UPDATE: have the initial empty window specially the dimensions

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
-#include <cstdlib>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
 
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+int main(int argc, char* argv[]) {
+  int w = 1000, h = 700;
+  const char* title = "Password Manager";
+  Fl_Window* window = new Fl_Window(w, h, title);
+  window->end();
+  window->show(argc, argv);
+  return Fl::run();
+}
