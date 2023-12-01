1031db306d90764c257da1c4568143daa9d53c09
Iñaki Pujol <ipujol10@gmail.com>
2023-12-01 17:34:02 +0100

    UPDATE: format code

--- libs/GUI/include/GUI/windows.hpp
+++ libs/GUI/include/GUI/windows.hpp
@@ -3,5 +3,5 @@
 #include <FL/Fl_Window.H>
 
 namespace window {
-Fl_Window* password(int W, int H, const char* title);
+Fl_Window *password(int W, int H, const char *title);
 }
--- libs/GUI/src/windows.cpp
+++ libs/GUI/src/windows.cpp
@@ -2,13 +2,13 @@
 #include <FL/Fl_Secret_Input.H>
 
 namespace window {
-Fl_Window* password(int W, int H, const char* title) {
-    Fl_Window* window = new Fl_Window(W, H, title);
-    int w = 300, h = 25;
-    int x = (W - w) / 2;
-    int y = (H - h) / 2;
-    Fl_Secret_Input* password = new Fl_Secret_Input(x, y, w, h, "Password");
-    window->end();
-    return window;
+Fl_Window *password(int W, int H, const char *title) {
+  Fl_Window *window = new Fl_Window(W, H, title);
+  int w = 300, h = 25;
+  int x = (W - w) / 2;
+  int y = (H - h) / 2;
+  Fl_Secret_Input *password = new Fl_Secret_Input(x, y, w, h, "Password");
+  window->end();
+  return window;
 }
-}
+} // namespace window
