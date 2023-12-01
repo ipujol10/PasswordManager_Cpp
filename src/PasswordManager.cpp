cb1e37915b8b557272a88b7172b878a0b82a61c9
Iñaki Pujol <ipujol10@gmail.com>
2023-12-01 17:34:02 +0100

    UPDATE: format code

--- src/PasswordManager.cpp
+++ src/PasswordManager.cpp
@@ -1,10 +1,10 @@
 #include <FL/Fl.H>
 #include <GUI/windows.hpp>
 
-int main (int argc, char *argv[]) {
-    int w = 1000, h = 700;
-    const char* title = "Password Manager";
-    auto password = window::password(w, h, title);
-    password->show(argc, argv);
-    return Fl::run();
+int main(int argc, char *argv[]) {
+  int w = 1000, h = 700;
+  const char *title = "Password Manager";
+  auto password = window::password(w, h, title);
+  password->show(argc, argv);
+  return Fl::run();
 }
