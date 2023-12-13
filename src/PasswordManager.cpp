395e336c5b938e2b05b81ff09f728447ff3f90ba
Iñaki Pujol <ipujol10@gmail.com>
2023-12-13 18:07:19 +0100

    UPDATE: be able to find a desired child from a group

--- src/PasswordManager.cpp
+++ src/PasswordManager.cpp
@@ -1,10 +1,24 @@
+#include "FL/Fl_Window.H"
 #include <FL/Fl.H>
 #include <GUI/windows.hpp>
+#include <cstring>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
   int w = 1000, h = 700;
   const char *title = "Password Manager";
+  Fl_Window *window = new Fl_Window(w, h, title);
   auto password = window::password(w, h, title);
-  password->show(argc, argv);
+  auto password2 = window::password2(w, h, "Change");
+  password->show();
+  password2->hide();
+  for (int i = 0; i < password->children(); i++) {
+    Fl_Widget *child = password->child(i);
+    if (strcmp((char *)child->user_data(), "Button") == 0) {
+      std::cout << "Found\n";
+    }
+  }
+  window->show();
+  window->show(argc, argv);
   return Fl::run();
 }
