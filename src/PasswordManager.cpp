495728a61f9c6ecf519a88024a0000fde84ef6b2
Iñaki Pujol <ipujol10@gmail.com>
2023-12-16 12:53:33 +0100

    UPDATE: be able to switch windows switch groups in the same window

--- src/PasswordManager.cpp
+++ src/PasswordManager.cpp
@@ -1,7 +1,6 @@
-#include "FL/Fl_Window.H"
+#include <FL/Fl_Window.H>
 #include <FL/Fl.H>
 #include <GUI/windows.hpp>
-#include <iostream>
 
 int main(int argc, char *argv[]) {
   int w = 1000, h = 700;
@@ -9,11 +8,15 @@ int main(int argc, char *argv[]) {
   Fl_Window *window = new Fl_Window(w, h, title);
   auto password = window::password(w, h, title);
   auto password2 = window::password2(w, h, "Change");
+  Fl_Group *groups[2] = {password, password2};
   password->show();
   password2->hide();
-  if (window::getWidget(password, "button")) {
-    std::cout << "Found\n";
-  }
+  window::getWidget(password, "button")
+      .value()
+      ->callback(window::go2window2, groups);
+  window::getWidget(password2, "button")
+      .value()
+      ->callback(window::go2window1, groups);
   window->show();
   window->show(argc, argv);
   return Fl::run();
