44052615c359d36e07296b0c0d7cf4a9570a67f4
Iñaki Pujol <ipujol10@gmail.com>
2023-12-30 13:02:46 +0100

    UPDATE: make the window into a class to kep track of all the attr

--- libs/GUI/include/GUI/windows.hpp
+++ libs/GUI/include/GUI/windows.hpp
@@ -1,12 +1,23 @@
 #pragma once
 
-#include "FL/Fl_Widget.H"
+#include <FL/Fl_Window.H>
 #include <FL/Fl_Group.H>
 
 namespace window {
-Fl_Group *password(int W, int H, const char *title);
-Fl_Group *password2(int W, int H, const char *title);
-Fl_Widget *getWidget(Fl_Group *group, const char *user_data);
-void go2window1(Fl_Widget *w, void *v);
-void go2window2(Fl_Widget *w, void *v);
+class MainWindow: public Fl_Window {
+private:
+  Fl_Group *password_group;
+  Fl_Group *password_group2;
+
+  Fl_Group *password(int W, int H, const char *title);
+  Fl_Group *password2(int W, int H, const char *title);
+  static void go2pass1(Fl_Widget *w, void *v);
+  static void go2pass2(Fl_Widget *w, void *v);
+
+public:
+  MainWindow(int width, int height, const char *title);
+  ~MainWindow();
+
+  Fl_Window *help;
+};
 } // namespace window
