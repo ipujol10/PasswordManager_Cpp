a615f4219ceb685686a370516ab2a9a711849681
Iñaki Pujol <ipujol10@gmail.com>
2025-10-20 18:52:56 +0200

    UPDATE: Implement the CopyString method

--- libs/GUI/include/GUI/widgets.hpp
+++ libs/GUI/include/GUI/widgets.hpp
@@ -11,6 +11,8 @@ class PasswordEntry : public Fl_Pack {
   Fl_Box* username_;
   const char *name_s_, *username_s_;
 
+  const char* CopyString(const std::string& str) const noexcept;
+
  public:
   PasswordEntry(const std::string& name, const std::string& sername, int w, int h,
                 const char* label = nullptr);
--- libs/GUI/src/widgets.cpp
+++ libs/GUI/src/widgets.cpp
@@ -3,16 +3,16 @@
 #include <FL/Enumerations.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Pack.H>
-#include <string.h>  //NOLINT
 
+#include <cstring>
 #include <string>
 
 namespace widgets {
 PasswordEntry::PasswordEntry(const std::string& name, const std::string& username, int w,
                              int h, const char* label)
     : Fl_Pack(0, 0, w, h, label),
-      name_s_(strdup(name.c_str())),
-      username_s_(strdup(username.c_str())) {
+      name_s_(CopyString(name)),
+      username_s_(CopyString(username)) {
   type(Fl_Pack::HORIZONTAL);
   name_ = new Fl_Box(Fl_Boxtype::FL_BORDER_BOX, 0, 0, w / 2, h, name_s_);
   username_ = new Fl_Box(Fl_Boxtype::FL_BORDER_BOX, 0, 0, w / 2, h, username_s_);
@@ -21,6 +21,14 @@ PasswordEntry::PasswordEntry(const std::string& name, const std::string& usernam
 
 PasswordEntry::~PasswordEntry() {
   delete name_;
+  delete[] name_s_;
   delete username_;
+  delete[] username_s_;
+}
+
+const char* PasswordEntry::CopyString(const std::string& str) const noexcept {
+  char* name = new char[str.length() + 1];
+  strcpy(name, str.c_str());
+  return name;
 }
 }  // namespace widgets
